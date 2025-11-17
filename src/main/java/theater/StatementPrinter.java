0bb1fec627c30ca30367807b231516f6a7ee2756
Olivier-Denis847 <olivierphilippedenis@gmail.com>
2025-11-17 15:46:48 -0500

    Task 2.1 - Refactored StatementPrinter.java

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -47,9 +47,6 @@ public class StatementPrinter {
 
         for (Performance p : invoice.getPerformances()) {
 
-            int thisAmount = 0;
-            thisAmount = getAmount(p);
-
             // add volume credits
             volumeCredits += Math.max(p.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
             // add extra credit for every five comedy attendees
@@ -59,8 +56,8 @@ public class StatementPrinter {
 
             // print line for this order
             result.append(String.format("  %s: %s (%s seats)%n", getPlay(p).getName(),
-                    frmt.format(thisAmount / Constants.PERCENT_FACTOR), p.getAudience()));
-            totalAmount += thisAmount;
+                    frmt.format(getAmount(p) / Constants.PERCENT_FACTOR), p.getAudience()));
+            totalAmount += getAmount(p);
         }
         result.append(String.format("Amount owed is %s%n", frmt.format(totalAmount / Constants.PERCENT_FACTOR)));
         result.append(String.format("You earned %s credits%n", volumeCredits));
